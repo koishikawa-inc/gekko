307dc022690baabb066c2f6ccdb5dfa06037ada7
Hiroto Sawada <h-sawada@koishikawa.co.jp>
2022-11-22 09:12:24 +0900

    プレビュー作り込み

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Gekko from './gekko';
 
-const gekko = new Gekko({ speed: 3000, isDuration: true, header: '.header', offset: 0 });
+const gekko = new Gekko({ speed: 1000, isDuration: true, header: '.header', offset: 0 });
 console.log(gekko);
 
 // gekko.scroll("#section-04");
@@ -9,8 +9,29 @@ console.log(gekko);
 gekko.on('beforeScroll', (anchor) => {
   console.log('beforeScroll', anchor);
 });
+
 gekko.on('afterScroll', (anchor) => {
   console.log('afterScroll', anchor);
+});
+
 document.getElementById('set')?.addEventListener('click', function () {
   gekko.options({ speed: 500 });
+});
+
+document.getElementById('options')?.addEventListener('submit', function (e: SubmitEvent) {
+  e.preventDefault();
+
+  const op: { [key: string]: any } = {};
+  const data = new FormData(e.target as HTMLFormElement);
+  data.forEach((value, key) => {
+    if (key === 'isDuration') {
+      op[key] = value === 'on';
+    } else {
+      op[key] = isNaN(Number(value)) ? value : Number(value);
+    }
+  });
+
+  console.log(op);
+
+  gekko.options(op);
 });
